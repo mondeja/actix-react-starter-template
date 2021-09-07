3058fa60b7b3635f3ed91360e98115f0b8b5ab9a
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2021-09-07 19:26:19 +0200

    Improve comments

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   // The root of your source code, typically /src
-  // `<rootDir>` is a token Jest substitutes
+  // `<rootDir>` is a token that Jest substitutes
   roots: ["<rootDir>/src"],
 
-  // Jest transformations -- this adds support for TypeScript using ts-jest
+  // Jest transformations
+  // Adds support for TypeScript using ts-jest
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
