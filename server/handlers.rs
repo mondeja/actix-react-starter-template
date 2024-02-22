daf81c138414e4adb2201445e1cdf48fed9c4e81
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-02-22 05:34:01 +0100

    Upgrade tooling and dependencies

--- server/handlers.rs
+++ server/handlers.rs
@@ -20,8 +20,6 @@ pub fn client_docs() -> Files {
 ///
 /// This must be the latest mount path in the application because, setted at root ('/'),
 /// services registered after this one will be inaccessible.
-///
-/// See https://docs.rs/actix-files/0.5.0/actix_files/struct.Files.html#impl
 pub fn index() -> Files {
     Files::new("/", "./client/build/").index_file("index.html")
 }
@@ -45,34 +43,19 @@ mod tests {
 
         // assert response contents
         let req = test::TestRequest::get().uri("/hello/world").to_request();
-        let result = test::read_response(&mut app, req).await;
+        let result = test::call_and_read_body(&mut app, req).await;
         assert_eq!(result, Bytes::from_static(b"Hello world!"));
 
         let req = test::TestRequest::get().uri("/hello/mondeja").to_request();
-        let result = test::read_response(&mut app, req).await;
+        let result = test::call_and_read_body(&mut app, req).await;
         assert_eq!(result, Bytes::from_static(b"Hello mondeja!"));
 
         Ok(())
-
-        // -----------------------------------------
-        // Other way to test this:
-        //let mut resp = app.call(req).await.unwrap();
-        //assert_eq!(resp.status(), http::StatusCode::OK);
-
-        // assert body content
-        //let body = resp.take_body();
-        //let body = body.as_ref().unwrap();
-        //assert_eq!(
-        //    &Body::from(b"Hello world!".as_ref()), // or serde.....
-        //    body
-        //);
-        // -----------------------------------------
     }
 
     #[actix_rt::test]
     async fn test_index() -> Result<(), Error> {
-        let app = App::new().service(index());
-        let mut app = test::init_service(app).await;
+        let app = test::init_service(App::new().service(index())).await;
 
         // assert response status
         let req = test::TestRequest::get().uri("/index.html").to_request();
